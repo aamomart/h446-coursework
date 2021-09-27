4806b7fa71a8db123ae864ab2c5a15ca8a28417c
Martin Aamodt-Leeper <martinaamodt-leeper@Martins-MacBook-Pro.local>
2021-09-27 19:26:44 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
 from collision import rectCollision
+import turtle
 
-x1 = 2
-y1 = 3
-w1 = 2
-h1 = 2
+colour = 'white'
 
-x2 = 3
-y2 = 2
-w2 = 2
-h2 = 2
+x1 = 20
+y1 = 30
+w1 = 20
+h1 = 20
+
+x2 = 30
+y2 = 20
+w2 = 20
+h2 = 20
 
 if rectCollision(x2, y2, w2, h2, x1, y1, w1, h1):
-    print("Collision")
+    colour = 'red'
 else:
-    print("No collision")
+    colour = 'green'
+
+t = turtle.Turtle()
+
+t.hideturtle()
+
+t.penup()
+
+t.forward(x1)
+t.left(90)
+t.forward(y1)
+t.forward(h1/2)
+
+t.pendown()
+
+t.fillcolor(colour)
+t.begin_fill()
+
+t.right(90)
+t.forward(w1/2)
+t.right(90)
+t.forward(h1)
+t.right(90)
+t.forward(w1)
+t.right(90)
+t.forward(h1)
+t.right(90)
+t.forward(w1/2)
+t.right(90)
+
+t.penup()
+
+t.forward(h1/2 + y1)
+t.right(90)
+t.forward(x1)
+t.right(180)
+t.forward(x2)
+t.left(90)
+t.forward(y2 + h2/2)
+t.right(90)
+
+t.pendown()
+
+t.fillcolor(colour)
+t.begin_fill()
+
+t.forward(w2/2)
+t.right(90)
+t.forward(h2)
+t.right(90)
+t.forward(w2)
+t.right(90)
+t.forward(h2)
+t.right(90)
+t.forward(w2/2)
+
+
+turtle.done()
