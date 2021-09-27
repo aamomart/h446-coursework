7094a5ba546baf3c2200ac469ebfa52563893dd4
Martin Aamodt-Leeper <martinaamodt-leeper@Martins-MacBook-Pro.local>
2021-09-27 16:54:46 +0100

    render.py

---
+++ render.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle
+
+def triangle1(x1, y1, h1, w1):
+    
+    t.penup()
+    t.forward(x1)
+    t.left(90)
+    t.forward(y1)
+    t.pendown()
+    
+    
+triangle1()
